b4c9d5e9cda8aa1e6785cbe9959d45c0aea907af
ErickRGL <erickguzmanlastra@gmail.com>
2018-06-13 21:13:06 -0500

    removed piece of code

--- controllers/register.js
+++ controllers/register.js
@@ -11,19 +11,6 @@ const handleRegister = (req, res, db, bcrypt) => {
         email: email
       })
       .into('login')
-      .returning('email')
-      .then(loginEmail => {
-        return trx('users')
-          .returning('*')
-          .insert({
-            email: loginEmail[0],
-            name: name,
-            joined: new Date()
-          })
-          .then(user => {
-            res.json(user[0]);
-          })
-      })
       .then(trx.commit)
       .catch(trx.rollback)
     })
